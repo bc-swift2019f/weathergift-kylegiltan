a8ea2635d7d991927975ff5f2481a3a911a49605
Kyle <32943027+kylegiltan@users.noreply.github.com>
2019-10-11 15:14:11 -0700

    7.2

--- WeatherGift/DetailVCViewController.swift
+++ WeatherGift/DetailVCViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailVCViewController.swift
 //  WeatherGift
 //
 //  Created by Kyle Gil Tan on 10/11/19.
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailVCViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
-
