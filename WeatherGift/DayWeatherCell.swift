15781c9d2d9173914bb3b74c522c4c5a8b01800c
Kyle <32943027+kylegiltan@users.noreply.github.com>
2019-10-26 17:10:45 -0400

    7.20

---
+++ WeatherGift/DayWeatherCell.swift
@@ -0,0 +1,30 @@
+//
+//  DayWeatherCell.swift
+//  WeatherGift
+//
+//  Created by Kyle Gil Tan on 10/26/19.
+//  Copyright © 2019 Kyle Gil Tan. All rights reserved.
+//
+
+import UIKit
+
+class DayWeatherCell: UITableViewCell {
+
+    @IBOutlet weak var dayCellIcon: UIImageView!
+    @IBOutlet weak var dayCellWeekday: UILabel!
+    @IBOutlet weak var dayCellMaxTemp: UILabel!
+    @IBOutlet weak var dayCellMinTemp: UILabel!
+    @IBOutlet weak var dayCellSummary: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
